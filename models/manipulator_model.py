cea22a82f8a77b9d6cfb279c9b18cbf1e19474d6
JaroslawBozek <jaroslaw.bozek@student.put.poznan.pl>
2020-05-20 15:09:59 +0200

    Manipulator_model update

--- models/manipulator_model.py
+++ models/manipulator_model.py
@@ -15,19 +15,28 @@ class ManiuplatorModel:
         self.m3 = 0.0
         self.r3 = 0.01
         self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
+        self.alpha = ((self.m1*pow(self.r1,2))+self.I_1+self.m2*(pow(self.l1, 2)+pow(self.r2, 2))+self.I_2)
+        self.beta = self.m2*self.l1*self.r2
+        self.gamma = (self.m2*pow(self.r2, 2))+self.I_2
 
     def M(self, x):
-        """
-        Please implement the calculation of the mass matrix, according to the model derived in the exercise
-        (2DoF planar manipulator with the object at the tip)
-        """
         q1, q2, q1_dot, q2_dot = x
-        return NotImplementedError()
+
+        M = np.array([[0,0],[0,0]])
+        M[0][0] = self.alpha+2*self.beta*np.cos(q2)
+        M[0][1] = self.gamma+self.beta*np.cos(q2)
+        M[1][0] = self.gamma+self.beta*np.cos(q2)
+        M[1][1] = self.gamma
+
+        return M
 
     def C(self, x):
-        """
-        Please implement the calculation of the Coriolis and centrifugal forces matrix, according to the model derived
-        in the exercise (2DoF planar manipulator with the object at the tip)
-        """
         q1, q2, q1_dot, q2_dot = x
-        return NotImplementedError()
+
+        C = np.array([[0,0],[0,0]])
+        C[0][0] = -self.beta*np.sin(q2)*q2_dot
+        C[0][1] = -self.beta*np.sin(q2)*(q1_dot+q2_dot)
+        C[1][0] = self.beta*np.sin(q2)*q1_dot
+        C[1][1] = 0
+
+        return C
